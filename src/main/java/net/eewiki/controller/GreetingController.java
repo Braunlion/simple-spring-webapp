5406b39272102cbafc145a1c7451a41ccb4fa969
Braunlion <braunlion@yahoo.com>
2023-07-18 12:40:01 -0400

    changed greeting to hello world

--- src/main/java/net/eewiki/controller/GreetingController.java
+++ src/main/java/net/eewiki/controller/GreetingController.java
@@ -43,7 +43,7 @@ public class GreetingController {
 		log.info("Calling getDefaultGreetingView()");
 		
 		// our greeting is "Hello World!"
-		m.addAttribute("greeting", "Welcome to DEVELOPER WORKFLOW!");
+		m.addAttribute("greeting", "Hello World!");
 		
 		return "greeting";
 	}
