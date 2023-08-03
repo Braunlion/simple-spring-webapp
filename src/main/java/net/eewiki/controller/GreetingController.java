970dd60a76cfde9a60aa588eed17fa00ca15fa31
Braunlion <braunlion@yahoo.com>
2023-08-03 12:34:01 -0400

    webhook testing 4

--- src/main/java/net/eewiki/controller/GreetingController.java
+++ src/main/java/net/eewiki/controller/GreetingController.java
@@ -43,7 +43,7 @@ public class GreetingController {
 		log.info("Calling getDefaultGreetingView()");
 		
 		// our greeting is "Hello World!"
-		m.addAttribute("greeting", "CICD is working now!");
+		m.addAttribute("greeting", "CICD is working !");
 		
 		return "greeting";
 	}
