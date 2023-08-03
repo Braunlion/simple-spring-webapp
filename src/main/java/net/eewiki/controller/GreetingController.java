2e6a3b74155cea89fe9eba93b28b9465a30f3f29
Braunlion <braunlion@yahoo.com>
2023-08-03 12:46:59 -0400

    webhook testing 6

--- src/main/java/net/eewiki/controller/GreetingController.java
+++ src/main/java/net/eewiki/controller/GreetingController.java
@@ -43,7 +43,7 @@ public class GreetingController {
 		log.info("Calling getDefaultGreetingView()");
 		
 		// our greeting is "Hello World!"
-		m.addAttribute("greeting", "CICD is working after port 80 is open !");
+		m.addAttribute("greeting", "CICD is working after port 80 is open on jenkins vm !");
 		
 		return "greeting";
 	}
